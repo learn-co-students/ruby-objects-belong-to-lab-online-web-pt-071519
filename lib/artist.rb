4a5105f89000c402e392427dfd3330127a66fd94
LaNice Powell <lanicemp@aol.com>
2019-08-12 14:42:59 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+#class Artist 
+  
+  #attr_accessor :name 
+  
+ # def initialize(name)
+   # @name = name 
+  #end 
+# 
+
+class Artist
+ 
+  attr_accessor :name
+ 
+end
+ 
+#beyonce = Artist.new("Beyonce")
